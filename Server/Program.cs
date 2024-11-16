803555fb03cfaea4ef42e61744c46f46080ab187
Ordinary56 <tbalogh695@gmail.com>
2024-11-16 10:18:27 +0100

    Added generic hosting to server

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,29 +1,61 @@
-﻿using System.Net;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
-namespace Server {
-    public class Program {
-        public static async Task Main(string[] args) {
-            Server server = new();
-            await server.StartAsync();
-        }
-    }
-    public class Server {
-        // Main server socket
-        readonly TcpListener _listener;
-        // List to store 
-        readonly List<(string,TcpClient)> _clients;
-        // Address
-        const string ADDRESS = "127.0.0.1";
-        // Port
-        const int PORT = 37065;
-        public Server()
+namespace Server
+{
+    public class Program()
+    {
+        static IHost _host = Host.CreateDefaultBuilder()
+                            .ConfigureServices(ConfigureServices)
+                            .ConfigureLogging(ConfigureLogging)
+                            .Build();
+
+        public static async Task Main()
         {
-            _listener = new(IPAddress.Parse(ADDRESS),PORT);
-            _clients = [];
+            Server server = _host.Services.GetRequiredService<Server>();
+            await server.StartAsync();
+            await _host.RunAsync();
         }
 
-        async Task<string> GetUsernameAsync(TcpClient client) 
+        static void ConfigureLogging(HostBuilderContext context, ILoggingBuilder builder)
+        {
+            builder.ClearProviders();
+            builder.AddConsole();
+            builder.AddDebug();
+        }
+        static void ConfigureServices(HostBuilderContext context, IServiceCollection services) 
+        {
+            services.AddSingleton<Server>();
+        }
+
+    }
+    public class Server
+    {
+        // Main server socket
+        readonly TcpListener _listener;
+
+        // List to store all clients 
+        readonly List<(string, TcpClient)> _clients;
+
+        // Address
+        const string ADDRESS = "127.0.0.1";
+
+        // Port
+        const int PORT = 37065;
+
+        // Logging information
+        ILogger<Server> _logger;
+        public Server()
+        {
+            _listener = new(IPAddress.Parse(ADDRESS), PORT);
+            _clients = [];
+        }
+        public Server(ILogger<Server> logger) : this() { _logger = logger; }
+
+        async Task<string> GetUsernameAsync(TcpClient client)
         {
             NetworkStream stream = client.GetStream();
             byte[] buffer = new byte[1024];
@@ -32,29 +64,42 @@ namespace Server {
             return Encoding.UTF8.GetString(buffer);
         }
 
-        public async Task StartAsync() {
+        public async Task StartAsync()
+        {
             _listener.Start();
-            Console.WriteLine($"Server is running in {ADDRESS}:{PORT}");
-            while(true) {
-                // TODO: remove client if there's no pair and it's disconnected
-                TcpClient client = await _listener.AcceptTcpClientAsync();
-                string username = await GetUsernameAsync(client);
+            _logger.Log(LogLevel.Information, "Server is running at {Address}:{Port}", ADDRESS, PORT);
+            TcpClient? client = null;
+            string username;
+            while (true)
+            {
+                client = await _listener.AcceptTcpClientAsync();
+                username = await GetUsernameAsync(client);
                 lock (_clients)
                 {
                     _clients.Add((username, client));
-                    Console.WriteLine($"Client connected, total clients: {_clients.Count}");
-                    Console.WriteLine($"{_clients.Count}");
-                    if ((_clients.Count & 1) == 0) 
-                    {
-                        TcpClient first = _clients[_clients.Count - 2].Item2;
-                        TcpClient second = _clients[_clients.Count - 1].Item2;
-                        _ = HandleClientPairAsync(first, second);  
-                    }
+                    _logger.Log(LogLevel.Information, "Client connected, total clients: {clientCount}", _clients.Count);
+                }
+                break;
+            }
+            await WaitForConnectingPairAsync(username, client!);
+        }
+        async Task WaitForConnectingPairAsync(string username,TcpClient client)
+        {
+            while(client.IsConnected())
+            {
+                if ((_clients.Count & 1) == 0)
+                {
+                    TcpClient first = _clients[_clients.Count - 2].Item2;
+                    TcpClient second = _clients[_clients.Count - 1].Item2;
+                    await HandleClientPairAsync(first, second);
                 }
             }
+            _logger.LogWarning("Client without a pair disconnected");
+            _clients.Remove((username, client));
+            await Task.CompletedTask;
         }
 
-        async Task HandleClientPairAsync(TcpClient first, TcpClient second) 
+        async Task HandleClientPairAsync(TcpClient first, TcpClient second)
         {
             using (first)
             using (second)
@@ -65,21 +110,32 @@ namespace Server {
                 while (true)
                 {
                     bytes_read_1 = await stream_1.ReadAsync(first_buffer);
-                    if (bytes_read_1 == 0) break;
+                    if (bytes_read_1 == 0) 
+                    {
+                        _logger.LogWarning("Client 1 sent 0 bytes as message, breaking up the connection");
+                        break; 
+                    }
                     await stream_2.WriteAsync(first_buffer.AsMemory(0, bytes_read_1));
                     Console.WriteLine($"First client sent: {Encoding.UTF8.GetString(first_buffer)}");
                     bytes_read_2 = await stream_2.ReadAsync(second_buffer);
-                    if(bytes_read_2 == 0) break;
+                    if (bytes_read_2 == 0) 
+                    {
+                        _logger.LogWarning("Client 1 sent 0 bytes as message, breaking up the connection");
+                        break; 
+                    }
                     await stream_1.WriteAsync(second_buffer.AsMemory(0, bytes_read_2));
                     Console.WriteLine($"Second client sent: {Encoding.UTF8.GetString(second_buffer)}");
                 }
             }
-            lock (_clients) 
+            lock (_clients)
             {
+                _logger.LogInformation("Removing clients....");
                 // Remove both clients when they are disconnected
                 _clients.RemoveAll(x => x.Item2 == first || x.Item2 == second);
+
+
             }
-            Console.WriteLine("Client pair disconnected");
+            _logger.Log(LogLevel.Information, "Client pair disconnected");
         }
     }
 }
---
+++ Server/SocketUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    static internal class SocketUtils
+    {
+        /// <summary>
+        /// Constantly checks wether a client is still connected or not
+        /// </summary>
+        /// <param name="client">The connected client</param>
+        /// <returns>True - if the client is still available and connencted, False - otherwise or if there was an error when checking</returns>
+        public static bool IsConnected(this TcpClient client)
+        {
+            try
+            {
+                return client != null && client.Client != null &&
+                   !(client.Client.Poll(1, SelectMode.SelectRead) && client.Client.Available == 0);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
